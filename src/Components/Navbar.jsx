93771725fa9bf255138fa62e4d9a1b93879ac2b0
N-1218 <malusarenamrata88@gmail.com>
2026-06-17 23:36:35 +0530

    frontend

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,53 +3,85 @@ import intbuddylogo from "../assets/intbuddylogo.png";
 
 function Navbar() {
   return (
-    // Changed bg to white and added border-bottom for a clean look
-    <nav className="navbar navbar-expand-lg shadow-sm fixed-top bg-dark border-bottom">
-      <div className="container-fluid px-0">
-        
-        <Link className="navbar-brand ms-3 d-flex align-items-center" to="/">
-          <img 
-            src={intbuddylogo} 
-            alt="IntBuddy Logo" 
-            style={{ 
-              height: "90px", 
-              width: "auto", 
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm fixed-top border-bottom">
+      <div className="container-fluid">
+
+        {/* Logo */}
+        <Link className="navbar-brand d-flex align-items-center ms-2" to="/">
+          <img
+            src={intbuddylogo}
+            alt="IntBuddy Logo"
+            className="img-fluid"
+            style={{
+              height: "70px",
+              width: "auto",
               objectFit: "contain",
-              marginTop: "-20px",
-              marginBottom: "-20px" 
-            }}  
+            }}
           />
         </Link>
 
-        <button className="navbar-toggler me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        {/* Toggle Button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
+        {/* Navbar Content */}
         <div className="collapse navbar-collapse" id="navbarNav">
-        
-          <ul className="navbar-nav">
+
+          {/* Links */}
+          <ul className="navbar-nav mx-lg-3 mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link text-warning fw-semibold" to="/">Home</Link>
+              <Link className="nav-link text-warning fw-semibold" to="/">
+                Home
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-warning fw-semibold" to="/About">About</Link>
+              <Link className="nav-link text-warning fw-semibold" to="/About">
+                About
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-warning fw-semibold" to="/Contact" >Contact</Link>
+              <Link className="nav-link text-warning fw-semibold" to="/Contact">
+                Contact
+              </Link>
             </li>
           </ul>
 
-          <form className="d-flex mx-auto" style={{ width: "40%" }}>
-            <input className="form-control rounded-pill me-2 border-secondary" type="search" placeholder="Search..." />
-           
-            <button className="btn btn-warning rounded-pill px-4" type="submit">Search</button>
+          {/* Search Bar */}
+          <form
+            className="d-flex flex-grow-1 mx-lg-3 my-2 my-lg-0"
+            role="search"
+          >
+            <input
+              className="form-control rounded-pill me-2"
+              type="search"
+              placeholder="Search..."
+            />
+            <button className="btn btn-warning rounded-pill px-4" type="submit">
+              Search
+            </button>
           </form>
 
-          <div className="d-flex align-items-center me-3">
-          
-            <Link className="btn btn-outline-warning rounded-pill me-2 px-4" to="/Login">Login</Link>
-            
-            <Link className="btn btn-warning rounded-pill px-4" to="/Registration">Register</Link>
+          {/* Buttons */}
+          <div className="d-flex flex-column flex-lg-row gap-2 me-3 mt-2 mt-lg-0">
+            <Link
+              className="btn btn-outline-warning rounded-pill px-4"
+              to="/Login"
+            >
+              Login
+            </Link>
+
+            <Link
+              className="btn btn-warning rounded-pill px-4"
+              to="/Registration"
+            >
+              Register
+            </Link>
           </div>
 
         </div>
